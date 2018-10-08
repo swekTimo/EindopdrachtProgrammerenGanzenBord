b8886eb424b4e86a5415396655180bdc82cf0618
Unknown <amchelle@avans.nl>
2018-10-08 13:40:02 +0200

    [Fixed] the code in the client and the player classes

--- GanzenBord/GanzenBord/ClientGanzenbord.cs
+++ GanzenBord/GanzenBord/ClientGanzenbord.cs
@@ -17,9 +17,49 @@ namespace GanzenBord
             string message;
             TcpClient client = new TcpClient(GetLocalIPAddress(), 6666);
             message = ReadMessage(client);
-            int value = 0;
-            int.TryParse(message, out value);
-            player = new Player(value);
+            int.TryParse(message, out int value);
+            message = ReadMessage(client);
+            int RankValue;
+            int.TryParse(message, out RankValue);
+            player = new Player(value, RankValue);
+
+            int PlayerField = 0;
+            bool done = false;
+            while (!done)
+            {
+                message = ReadMessage(client);
+                if (message != "player 1 has won!"
+                    && message != "player 2 has won!"
+                    && message != "player 3 has won!"
+                    && message != "player 4 has won!")
+                {
+
+                    player.MoveGoose();
+                    if (PlayerField != player.Field)
+                        message = "SameTile";
+                    else
+                        message = player.Field.ToString();
+                    PlayerField = player.Field;
+
+
+                    message = ReadMessage(client);
+                    int.TryParse(message, out RankValue);
+                    player.Ranking = RankValue;
+
+
+                    if (player.hasWon)
+                        message = "endGame";
+                    else
+                        message = "KeepPlaying";
+                    WriteMessage(client, message);
+
+
+                    message = ReadMessage(client);
+                    if (message == "bye")
+                        done = true;
+                }
+                client.Close();
+            }
         }
 
 
